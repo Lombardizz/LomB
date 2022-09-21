557f7c73b72837606c56f168624a66961afb1503
Ikbar Adani <112878270+Lombardizz@users.noreply.github.com>
2022-09-21 21:40:52 +0700

    variable

---
+++ variable/variable.py
@@ -0,0 +1,14 @@
+#pemanggilan pertama
+a = 10
+b = 26
+panjang = 200
+
+print("nilai a = ", a)
+print("nilai b = ", b)
+print("nilai panjang = ", panjang)
+
+#Pemanggilan Kedua
+a = 100
+print("nilai a = ", a)
+b = a
+print("nilai b = ", b)
